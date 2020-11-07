7dbedb1bd4a5dc4872f58ca670c7190e225383ca
manaswinisreekar <manaswinikolluru@gmail.com>
2020-11-07 17:57:04 +0530

    added home and profile page

--- src/Controller.js
+++ src/Controller.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route } from 'react-router'
-
 import Login from './screens/login/Login';
+import Home from './screens/home/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Redirect } from 'react-router'
 
 class Controller extends Component {
 
-    constructor() {
-        super();
-        this.baseUrl = "v1/users/self/";
-    }
     render() {
         return (
             <Router>
                 <div className="main-container">
                     <Route exact path='/' render={(props) => <Login {...props} baseUrl={this.baseUrl} />} />
-                </div>
+                    <Route path='/home' render={(props) => (sessionStorage.getItem("access-token") !== null ?
+                        <Home {...props} baseUrl={this.baseUrl} loggedIn="true" showSearchTab="true" /> : <Redirect to="/" />)} />
+                    </div>
             </Router>
         )
     }
 }
 
-export default Controller; 
+export default Controller;
