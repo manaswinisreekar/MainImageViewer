76504cc6f6790990cb38f52faac550d4eac3af62
manaswinisreekar <manaswinikolluru@gmail.com>
2020-11-11 14:30:10 +0530

    generated 60 days accesstoken

--- src/common/Config.js
+++ src/common/Config.js
@@ -1,13 +1,13 @@
 //accesstoken
 //"access_token": "IGQVJWZA09tQ29LOTZAwbVNuemRNdEpDeGJNSjZAieFl2djFNWS0yRTYzdnJUREdjLWFVS0hGZAnlwenk2ZAm5JUi1SQlVJal80c3ZA3X3l3UVl4R2FvaTdsd1N2M0ZAIMENzc2NZASDdfTW5mMWUtcHVQTmM0dmNicTNPSjJxaWVn",
-//
+//acceetoken IGQVJVcjA4WjhtdnhDX3hwMktLaVBGVVA1aWlETmE2SloyejJScm9SNHhManE1RDVxdDc5NmxvaUxJd21qTzA3OUVmS1E5TDhFcDFEa3NNRUlBdmpwZAVBONmt0T2xYanJ1QWV3bHRB
 var Config = {
     "credentials": {
         "username": "user",
         "password": "user"
     },
     "authentication": {
-        "access-token": "IGQVJWZA09tQ29LOTZAwbVNuemRNdEpDeGJNSjZAieFl2djFNWS0yRTYzdnJUREdjLWFVS0hGZAnlwenk2ZAm5JUi1SQlVJal80c3ZA3X3l3UVl4R2FvaTdsd1N2M0ZAIMENzc2NZASDdfTW5mMWUtcHVQTmM0dmNicTNPSjJxaWVn"
+        "access-token": "IGQVJVcjA4WjhtdnhDX3hwMktLaVBGVVA1aWlETmE2SloyejJScm9SNHhManE1RDVxdDc5NmxvaUxJd21qTzA3OUVmS1E5TDhFcDFEa3NNRUlBdmpwZAVBONmt0T2xYanJ1QWV3bHRB"
     },
     "api":
     {
